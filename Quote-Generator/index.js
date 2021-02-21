e20622755e33d1a16bfe533cdc0f3033e39946b1
zachyboy-92 <zakaria_0456@hotmail.com>
2021-02-20 19:20:55 -0500

    First commit

--- Quote-Generator/index.js
+++ Quote-Generator/index.js
@@ -1,35 +1,96 @@
 "use strict";
 
-const btnSubmit = document.querySelector("#submit");
+const body = document.querySelector("body");
+const btnSubmit = document.querySelector("#generate");
 const options = document.querySelector("#quote__options");
+const quoteConainer = document.querySelector(".quote__container");
+const quote = document.querySelector(".quote");
+const quotesChild = document.querySelector(".quotes__child");
 
-const quoteOptions = ["quote", "advice", "jokes"];
+const quoteOptions = ["general", "advice", "jokes"];
 
 const api = {
-  quote: "https://api.quotable.io/random",
+  general: "https://api.quotable.io/random",
   advice: "https://api.adviceslip.com/advice",
   jokes: "https://official-joke-api.appspot.com/random_joke",
 };
+
+// fetch("https://official-joke-api.appspot.com/random_joke")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
 //Create random color generator
 let color = "";
 const randomColor = function () {
   let randomColorNum = Math.floor(Math.random() * 16777215).toString(16);
   color = `#${randomColorNum}`;
-  return color;
+  return (quoteConainer.style.backgroundColor = color);
 };
-console.log(randomColor());
+
+//API fetcher
+async function apiFetcher(api) {
+  try {
+    const getApi = await fetch(api);
+    const res = await getApi.json();
+    return res;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// apiFetcher(api.jokes).then((res) => res);
+
+//Generate General Quote
+const generalQuote = function () {
+  apiFetcher(api.general).then((res) => {
+    randomColor();
+    quote.textContent = res.content;
+    quotesChild.textContent = res.author;
+  });
+};
+
+//Generate Advice
+const adviceQuote = function () {
+  apiFetcher(api.advice).then((res) => {
+    randomColor();
+    quote.textContent = res.slip.advice;
+    quotesChild.textContent = "";
+  });
+};
+
+//Generate jokes
+const jokesQuote = function () {
+  apiFetcher(api.jokes).then((res) => {
+    randomColor();
+    quote.textContent = res.setup;
+    setTimeout(() => (quotesChild.textContent = res.punchline), 3000);
+  });
+};
+
+//clear container
+const clear = function () {
+  quote.textContent = "";
+  quotesChild.textContent = "";
+};
 
 //Generate random index to pick a quote
+const quoteArray = ["generalQuote", "adviceQuote", "jokesQuote"];
 let randomQuoteNum = Math.floor(Math.random() * 3);
-let randomQuote = api[quoteOptions[randomQuoteNum]];
-console.log(randomQuote);
+let randomQuote = quoteArray[randomQuoteNum];
 
-const getQuote = async function (quote) {
-  if (!quote) return;
-  const getUrl = await fetch(quote);
-  const res = await getUrl.json();
-  return res;
+const generateQuote = function (quote) {
+  if (quote === "generalQuote") {
+    generalQuote();
+  }
+  if (quote === "adviceQuote") {
+    adviceQuote();
+  }
+  if (quote === "jokesQuote") {
+    jokesQuote();
+  }
 };
 
-getQuote(randomQuote).then((quotes) => {});
+btnSubmit.addEventListener("click", function () {
+  clear();
+  generateQuote(randomQuote);
+});
